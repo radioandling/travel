7ee037995bc2f3dc0983e5710ce2d55931265673
radio528 <474775913@qq.com>
2020-03-11 13:34:38 +0800

    完成search的布局

--- src/components/pages/city/city.vue
+++ src/components/pages/city/city.vue
@@ -1,14 +1,18 @@
 <template>
 <div class='city'>
   <city-header></city-header>
+  <city-search></city-search>
 </div>
 </template>
 
 <script>
 import CityHeader from './cityHeader'
+import CitySearch from './citySearch'
+
 export default {
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   },
   data () {
     return {}
---
+++ src/components/pages/city/citySearch.vue
@@ -0,0 +1,30 @@
+<template>
+<div class='search'>
+  <input type="text" placeholder="请输入城市名">
+</div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang='less' scoped>
+.search{
+  height: .86rem;
+  background-color: #00bcd4;
+  line-height: .86rem;
+  text-align: center;
+  input{
+    width: 90%;
+    height: .6rem;
+    border-radius: 3px;
+    padding: 0 .2rem;
+    box-sizing: border-box;
+  }
+}
+</style>
